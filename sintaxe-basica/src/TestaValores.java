622f2046614fee22090364ecb9d00fc35a3fd6b7
Vania Almeida <vanialadev@gmail.com>
2018-06-24 19:15:32 -0300

    capítulo 5 - trabalhando com caracteres

---
+++ sintaxe-basica/src/TestaValores.java
@@ -0,0 +1,15 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int primeiro = 5;
+	    int segundo = 7;
+        segundo = primeiro;
+        primeiro = 10;
+	    
+	    System.out.println(segundo);
+
+	}
+
+}
