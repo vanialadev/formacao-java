fc2c31a6da2f92f3ab7055b8d6b677d387bbf58c
Vania Almeida <vanialadev@gmail.com>
2018-06-24 19:55:17 -0300

    capítulo 7 - controlando fluxo com laços

---
+++ sintaxe-basica/src/DesafioOpcional.java
@@ -0,0 +1,11 @@
+
+public class DesafioOpcional {
+
+	public static void main(String[] args) {
+
+		for (int numero = 3; numero < 100; numero+=3) {
+			System.out.print(numero + " ");
+		}
+	}
+
+}
---
+++ sintaxe-basica/src/DesafioOpcional2.java
@@ -0,0 +1,12 @@
+
+public class DesafioOpcional2 {
+
+	public static void main(String[] args) {	
+		int fatorial = 1;
+		for (int numero = 1; numero <= 10; numero++) {
+				fatorial *= numero;
+			System.out.println("fatorial de " + numero + "! = "  + fatorial);
+		}
+	}
+
+}
