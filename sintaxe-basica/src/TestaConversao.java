8acd0337f677d6fa4abca28d1885b96bc56dfdc0
Vania Almeida <vanialadev@gmail.com>
2018-06-24 19:03:12 -0300

    capítulo 4 - tipos e variáveis

---
+++ sintaxe-basica/src/TestaConversao.java
@@ -0,0 +1,26 @@
+
+public class TestaConversao {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        double salario = 1270.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+        
+        long numeroGrande = 32432423523L;
+
+        short valorPequeno = 2131;
+        byte b = 127;
+        
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1+valor2;
+        
+        float pontoFlutuante = 3.14f;
+
+
+	}
+
+}
---
+++ sintaxe-basica/src/TestaPontoFlutuante.java
@@ -0,0 +1,24 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double salario = 1250.70;
+        System.out.println("meu salário é " + salario);
+        
+        double divisao = 3.14 / 2;
+        System.out.println(divisao);
+        
+        int outraDivisao = 5 / 2;
+        System.out.println(outraDivisao);
+        
+        double novaTentativa = 5 / 2;
+        System.out.println(novaTentativa);
+
+        double outraTentativa = 5.0 / 2;
+        System.out.println(outraTentativa);
+
+	}
+
+}
